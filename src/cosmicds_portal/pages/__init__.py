91418a145edf909009a975fcb5c5c968cf90a0a9
Nicholas Earl <contact@nicholasearl.me>
2023-06-19 11:26:23 -0500

    Create basic interaction with logged in user

--- src/cosmicds_portal/pages/__init__.py
+++ src/cosmicds_portal/pages/__init__.py
@@ -1,8 +1,24 @@
+import ipyvuetify as v
 import solara
 from solara.alias import rv
-import ipyvuetify as v
+from solara_enterprise import auth
+
 from ..components.hero import Hero
 from ..components.login import Login
+
+# # required if you don't use the default test account
+# SOLARA_SESSION_SECRET_KEY="SECRETTESTKEY"
+# # found in the Auth0 dashboard Applications->Applications->Client ID
+# SOLARA_OAUTH_CLIENT_ID="ohpOLLdBibfGp2YUVwkmokTw18CXiD0B"
+# # found in the Auth0 dashboard Applications->Applications->Client secret
+# SOLARA_OAUTH_CLIENT_SECRET="DRmKDfn4ikzkdkSHXvCQPg6p0vQe46aCqmUND4YVQ9yzxZPrjdqp9qdMkGBl23MC"
+# # found in the Auth0 dashboard Applications->Applications->Domain
+# SOLARA_OAUTH_API_BASE_URL="dev-tbr72rd5whnwlyrg.us.auth0.com"
+# SOLARA_OAUTH_SCOPE="openid profile email"
+
+# # For testing locally
+# SOLARA_SESSION_HTTPS_ONLY= "false"
+
 
 v.theme.dark = True
 
@@ -13,7 +29,8 @@ def Page():
         with rv.Col(cols=6):
             rv.Html(tag="div", children="Our Mission", class_="display-1")
             # with rv.Html(tag="p", class_="subtitle-1"):
-            solara.Markdown("""
+            solara.Markdown(
+                """
 The world is fast-becoming a place driven by data.  To address dire shortages 
 of data-competency in the workforce, industry leaders are calling for 
 educational pathways that teach people how to interact with data.  The Cosmic 
@@ -23,11 +40,13 @@ through engaging, interactive data stories.
 The project facilitates connections between astronomers who want to tell the 
 story of a discovery and learners who can interrogate the data behind the 
 story on their own, using easy-to-use but powerful data science and 
-visualization techniques.""")
+visualization techniques."""
+            )
 
         with rv.Col(cols=6):
-            rv.Sheet(rounded='lg', color='#cccccc', class_="fill-height",
-                     min_height="20vh")
+            rv.Sheet(
+                rounded="lg", color="#cccccc", class_="fill-height", min_height="20vh"
+            )
 
     return main
 
@@ -41,9 +60,7 @@ def Layout(children=[]):
         solara.Title("Testing")
 
         with rv.AppBar(elevate_on_scroll=True, app=True):
-            with rv.Container(
-                    class_="fill-height d-flex align-center"):
-
+            with rv.Container(class_="fill-height d-flex align-center"):
                 with solara.Link(solara.resolve_path("/")):
                     rv.Avatar(class_="me-10 ms-4", color="#cccccc", size="32")
 
@@ -54,10 +71,36 @@ def Layout(children=[]):
 
                 rv.Spacer()
 
-                Login(text=True)
+                # Login(text=True)
 
-                with solara.Link(solara.resolve_path("/register")):
-                    rv.Btn(children=["Register"], depressed=True)
+                # with solara.Link(solara.resolve_path("/register")):
+                #     rv.Btn(children=["Register"], depressed=True)
+
+                if not auth.user.value:
+                    solara.Button(
+                        "Login",
+                        icon_name="mdi-logon",
+                        href=auth.get_login_url(),
+                        depressed=True,
+                    )
+                else:
+                    user_info = auth.user.value["userinfo"]
+
+                    if "name" in user_info:
+                        solara.Markdown(f"{user_info['name']}")
+
+                    with solara.Link(solara.resolve_path("/create_class")):
+                        rv.Btn(
+                            children=["Create Class"],
+                            outlined=True,
+                            color="green",
+                        )
+                    solara.Button(
+                        "Logout",
+                        icon_name="mdi-logout",
+                        href=auth.get_logout_url(),
+                        depressed=True,
+                    )
 
         with rv.Content():
             if level == 0:
@@ -80,6 +123,11 @@ def Layout(children=[]):
 
                 with rv.Row():
                     with rv.Col(class_="d-flex justify-center"):
-                        rv.Html(tag="p", children=["Copyright © 2021 The President and Fellows of Harvard College"])
+                        rv.Html(
+                            tag="p",
+                            children=[
+                                "Copyright © 2021 The President and Fellows of Harvard College"
+                            ],
+                        )
 
     return main
--- src/cosmicds_portal/pages/register/__init__.py
+++ src/cosmicds_portal/pages/register/__init__.py
@@ -1,35 +1,45 @@
+import re
+
 import solara
 from solara.alias import rv
-import re
+from solara_enterprise import auth
 
 
 @solara.component
 def Page():
-    valid, set_valid = solara.use_state({
-      "valid": False,
-      "firstname": "",
-      "lastname": "",
-      "nameRules": [
-        lambda v: v or 'Name is required',
-        lambda v: len(v) <= 10 or 'Name must be less than 10 characters',
-      ],
-      "email": "",
-      "emailRules": [
-        lambda v: v or 'E-mail is required',
-        lambda v: re.match("/.+@.+/", v) or 'E-mail must be valid',
-      ],
-    })
+    if not auth.user.value:
+        solara.Button("DO IT", icon_name="mdi-logon", href=auth.get_login_url())
+    else:
+        user_info = auth.user.value["userinfo"]
+        
+        if "name" in user_info:
+            solara.Markdown(f"### Welcome {user_info['name']}")
+        solara.Button("Logout", icon_name="mdi-logout", href=auth.get_logout_url())
+    # valid, set_valid = solara.use_state({
+    #   "valid": False,
+    #   "firstname": "",
+    #   "lastname": "",
+    #   "nameRules": [
+    #     lambda v: v or 'Name is required',
+    #     lambda v: len(v) <= 10 or 'Name must be less than 10 characters',
+    #   ],
+    #   "email": "",
+    #   "emailRules": [
+    #     lambda v: v or 'E-mail is required',
+    #     lambda v: re.match("/.+@.+/", v) or 'E-mail must be valid',
+    #   ],
+    # })
 
-    with rv.Form() as main:
-        rv.Html(tag='div', children=['Create an Account'], class_="display-1")
+    # with rv.Form() as main:
+    #     rv.Html(tag='div', children=['Create an Account'], class_="display-1")
 
-        with rv.Container():
-            with rv.Row():
-                with rv.Col(cols=12):
-                    rv.TextField(v_model=valid['firstname'],
-                                 #rules=valid['nameRules'],
-                                 counter="10",
-                                 label="First name",
-                                 required=True)
+    #     with rv.Container():
+    #         with rv.Row():
+    #             with rv.Col(cols=12):
+    #                 rv.TextField(v_model=valid['firstname'],
+    #                              #rules=valid['nameRules'],
+    #                              counter="10",
+    #                              label="First name",
+    #                              required=True)
 
-    return main
+    # return main
