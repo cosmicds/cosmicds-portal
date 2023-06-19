f989c08d1a0ba782fa686726f29704495a9f66b9
Nicholas Earl <contact@nicholasearl.me>
2023-06-19 11:26:50 -0500

    Setup support for external authentication

--- src/cosmicds_portal/server.py
+++ src/cosmicds_portal/server.py
@@ -1,10 +1,14 @@
-from fastapi import Depends, FastAPI
+import httpx
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_login import LoginManager
 from fastapi_login.exceptions import InvalidCredentialsException
 from solara.server.fastapi import app as solapp
 
-SECRET = "super-secret-key"
+CLIENT_ID = "your-client-id"
+CLIENT_SECRET = "your-client-secret"
+CALLBACK_URL = "http://your-callback-url"
 
 app = FastAPI()
 # manager = LoginManager(SECRET, '/login',
@@ -15,6 +19,50 @@ app = FastAPI()
 @app.get("/hello")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/auth")
+def login(request: Request):
+    # Generate the CILogon authorization URL
+    authorization_url = f"https://cilogon.org/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={CALLBACK_URL}"
+
+    # Redirect the user to the CILogon authentication page
+    return RedirectResponse(authorization_url)
+
+
+@app.get("/auth/callback")
+async def callback(code: str, request: Request):
+    # Exchange the authorization code for an access token
+    token_endpoint = "https://cilogon.org/oauth2/token"
+    token_params = {
+        "grant_type": "authorization_code",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "redirect_uri": CALLBACK_URL,
+        "code": code,
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(token_endpoint, data=token_params)
+
+    response.raise_for_status()
+    access_token = response.json()["access_token"]
+
+    # Retrieve user information using the access token
+    userinfo_endpoint = "https://cilogon.org/oauth2/userinfo"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(userinfo_endpoint, headers=headers)
+
+    response.raise_for_status()
+    userinfo = response.json()
+
+    # Process user information and create a session for the user
+    # (Note: This is a simplified example, you may need to implement your own logic here)
+
+    # Return a response to the authenticated user
+    return {"message": f"Hello, {userinfo['name']}!"}
 
 
 app.mount("/", app=solapp)
