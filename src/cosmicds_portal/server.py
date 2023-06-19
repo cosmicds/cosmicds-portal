44585a312abb8e41a806c76c2e7282e114c7b9ea
Nicholas Earl <contact@nicholasearl.me>
2023-06-19 15:47:54 -0500

    Preliminary implementation of fastapi users

--- src/cosmicds_portal/server.py
+++ src/cosmicds_portal/server.py
@@ -1,19 +1,65 @@
 import httpx
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI
+from fastapi import Request
 from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi_login import LoginManager
-from fastapi_login.exceptions import InvalidCredentialsException
 from solara.server.fastapi import app as solapp
+
+from .db import User, create_db_and_tables
+from .schemas import UserCreate, UserRead, UserUpdate
+from .users import (
+    SECRET,
+    auth_backend,
+    current_active_user,
+    fastapi_users,
+    google_oauth_client,
+)
 
 CLIENT_ID = "your-client-id"
 CLIENT_SECRET = "your-client-secret"
 CALLBACK_URL = "http://your-callback-url"
 
 app = FastAPI()
-# manager = LoginManager(SECRET, '/login',
-#                        use_cookie=True,
-#                        use_header=False)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt",
+    tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+app.include_router(
+    fastapi_users.get_oauth_router(google_oauth_client, auth_backend, SECRET),
+    prefix="/auth/google",
+    tags=["auth"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
 
 
 @app.get("/hello")
