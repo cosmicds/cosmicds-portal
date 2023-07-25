a7508545d4414ee44c1a985db7d1ad628f0426bb
Nicholas Earl <contact@nicholasearl.me>
2023-07-25 15:57:05 -0500

    Remove server stuff

--- src/cosmicds_portal/server.py
+++ src/cosmicds_portal/server.py
@@ -4,62 +4,11 @@ from fastapi import Request
 from fastapi.responses import RedirectResponse
 from solara.server.fastapi import app as solapp
 
-from .db import User, create_db_and_tables
-from .schemas import UserCreate, UserRead, UserUpdate
-from .users import (
-    SECRET,
-    auth_backend,
-    current_active_user,
-    fastapi_users,
-    google_oauth_client,
-)
-
 CLIENT_ID = "your-client-id"
 CLIENT_SECRET = "your-client-secret"
 CALLBACK_URL = "http://your-callback-url"
 
 app = FastAPI()
-
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt",
-    tags=["auth"]
-)
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
-app.include_router(
-    fastapi_users.get_oauth_router(google_oauth_client, auth_backend, SECRET),
-    prefix="/auth/google",
-    tags=["auth"],
-)
-
-
-@app.get("/authenticated-route")
-async def authenticated_route(user: User = Depends(current_active_user)):
-    return {"message": f"Hello {user.email}!"}
-
-
-@app.on_event("startup")
-async def on_startup():
-    # Not needed if you setup a migration system like Alembic
-    await create_db_and_tables()
 
 
 @app.get("/hello")
